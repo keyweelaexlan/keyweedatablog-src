fdc92780df0fa89e07e75cf6e1a1a8460442ba23
alex <lanalex@gmail.com>
2016-02-21 14:08:36 +0200

    initial

---
+++ content/article.md
@@ -0,0 +1,12 @@
+Title: Doc2Vec for facebook page text classification
+Date: 2010-12-03 10:20
+Modified: 2010-12-05 19:30
+Category: Text algorithms
+Tags: doc2vec, nlp, facebook
+Slug: doc2vec-label-classification
+Authors: Alex Lan
+
+
+{% notebook notebook.ipynb %}
+
+
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Alex Lan'
+EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+
+
+SITENAME = u'Keywee Data and Algorithms blog'
+SITEURL = ''
+THEME = '/Users/alex/keywee-new/pelican-themes/pelican-octopress-theme'
+PLUGIN_PATH = ['/Users/alex/keywee-new/pelican-plugins']
+PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.youtube', 'liquid_tags.vimeo',
+           'liquid_tags.include_code', 'liquid_tags.notebook']
+
+
+PATH = 'content'
+
+NOTEBOOK_DIR = 'notebooks'
+
+TIMEZONE = 'Europe/Paris'
+
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+GITHUB_USER='keyweelaexlan'
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
